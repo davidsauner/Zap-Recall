4961485e1d4c0a396c5f19108e97a31173df7ea4
David <davidsauner@gmail.com>
2022-12-05 17:33:26 -0300

    Fixed- data test

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <>
+      <h1>Hellooooo</h1>
+      <p>uiui</p>
+    </>
+  );
+}
--- src/data/Icon.js
+++ src/data/Icon.js
@@ -8,11 +8,11 @@ export default function Icon({ status }) {
       {status === "error" && (
         <img src={error} alt={error} data-test="no-icon" />
       )}
-      {status === "half" && <img src={half} alt={half} />}
-      {status === "zap" && <img src={zap} alt={zap} data-test="zap-icon" />}
-      {status === "none" && (
-        <img src={seta} alt={seta} data-test="partial-icon" />
+      {status === "half" && (
+        <img src={half} alt={half} data-test="partial-icon" />
       )}
+      {status === "zap" && <img src={zap} alt={zap} data-test="zap-icon" />}
+      {status === "none" && <img src={seta} alt={seta} data-test="play-btn" />}
     </>
   );
 }
